9920b9905da67afbaa72bb710c30c14043e3918c
Makovei Miron <miron.makovei@gmail.com>
2025-03-08 11:57:36 +0100

    hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from "./Login";
 import Dashboard from "./pages/Dashboard";
 import AdminPanel from "./pages/AdminPanel"; // Admin panel importálása
